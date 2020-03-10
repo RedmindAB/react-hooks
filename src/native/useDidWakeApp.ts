5bb470708dc30d4aea75dc6e54de1909205b77b1
Sascha Ringström <sascha.ringstrom@redmind.se>
2020-03-10 23:08:55 +0100

    added date related props to useDidWakeApp

--- src/native/useDidWakeApp.ts
+++ src/native/useDidWakeApp.ts
@@ -1,12 +1,27 @@
 import { AppState, AppStateStatus } from 'react-native'
 import { useEffect } from 'react'
 
-const useDidWakeApp = (callback: () => void): void => {
+export type UseDidWakeAppCallbackProps = {
+  latestActivityDate: Date
+  minutesSinceLastActivity: number
+}
+
+const useDidWakeApp = (
+  callback: (props: UseDidWakeAppCallbackProps) => void
+): void => {
   let prevAppState = ''
+  let latestActivity = new Date()
 
   const onChange = (state: AppStateStatus) => {
     if (prevAppState === 'background' && state === 'active') {
-      callback()
+      const latestActivityDate = new Date(latestActivity)
+      const minutesSinceLastActivity = Math.round(
+        (Date.now() - latestActivityDate.getTime()) / 1000 / 60
+      )
+
+      callback({ latestActivityDate, minutesSinceLastActivity })
+
+      latestActivity = new Date()
     }
 
     prevAppState = state
